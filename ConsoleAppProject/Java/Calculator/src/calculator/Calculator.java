8f489dca12ad78235758fac66f5acdcdcf16314a
Daniel Rayson <100173524@student.burnley.ac.uk>
2022-02-02 14:01:10 +0000

    Java Additions

---
+++ ConsoleAppProject/Java/Calculator/src/calculator/Calculator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author 100173524
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+
+    }
+    
+}
